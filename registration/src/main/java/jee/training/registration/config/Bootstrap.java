ee8058190fe596ee32fc6df0358143cd57b6ffe2
Falk Sippach <falk@jug-da.de>
2022-09-09 09:43:20 +0200

    bootstrap location and event data to db

--- registration/src/main/java/jee/training/registration/config/Bootstrap.java
+++ registration/src/main/java/jee/training/registration/config/Bootstrap.java
@@ -31,6 +31,18 @@ public class Bootstrap {
         Location frankfurt = new Location(2L, "Frankfurt");
         Location online = new Location(3L, "Online");
 
+        Event e1 = new Event("2022-09-22", parseDate("2022-09-22"), darmstadt);
+        Event e2 = new Event("2022-10-13", parseDate("2022-10-13"), online);
+        Event e3 = new Event("2022-11-10", parseDate("2022-11-10"), online);
+
+        if (em.createQuery("select count(e) from Event e", Long.class).getSingleResult() == 0) {
+            List.of(e1, e2, e3).forEach(e -> em.persist(e));
+        }
+
+        if (em.createQuery("select count(l) from Location l", Long.class).getSingleResult() == 0) {
+            List.of(darmstadt, frankfurt, online).forEach(l -> em.persist(l));
+        }
+
     }
 
     private Date parseDate(String date) {
